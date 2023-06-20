b50fc4e502d88bc0808be21d9c47fb7f4c8918ca
Guxim Selmani <guxim.selmani1@hotmail.com>
2023-06-20 12:51:44 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'antd'
+function App() {
+  return (
+    <div>
+      <h1>Jira clone!</h1>
+    </div>
+  );
+}
+
+export default App;
