885f7b5455e6859de7d8b72ede20ac8ca6eaf2d9
Miraxh Lika <ml28591@seeu.edu.mk>
2023-06-21 13:13:18 +0200

    feat: added single column component and set up all columns board

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -1,32 +1,26 @@
-import React from 'react'
-import {CalendarTwoTone } from '@ant-design/icons'
-import './Skeleton.css'
+import React from "react";
+import { CalendarTwoTone } from "@ant-design/icons";
+import "./Skeleton.css";
+import { SingleColumn } from "../SingleColumn/SingleColumn";
 export const Skeleton = () => {
+  const columnNames = ["TO DO", "IN PROGRESS", "CODE REVIEW", "QA", "DONE"];
   return (
-    <div className='container'>
-      <p className='title'><CalendarTwoTone /> Board</p>
-      <div className='board-tools'>
-        <div className='empty-board'>
-          Empty board btn
-        </div>
-        <div className='employee-avatars'>
-          Employees avatars
-        </div>
-        <div className='add-employee-icon'>
-          Icon
-        </div>
-        <div className='ordering'>
-          Ordering
-        </div>
-        <div className='create-btn'>
-          Create button
-        </div>
-      </div> 
-      <div className='board'>
-        {Array.from({length:5}).map((_)=>{
-          return <h1 className='board-item'>123</h1>
+    <div className="container">
+      <p className="title">
+        <CalendarTwoTone /> Board
+      </p>
+      <div className="board-tools">
+        <div className="empty-board">Empty board btn</div>
+        <div className="employee-avatars">Employees avatars</div>
+        <div className="add-employee-icon">Icon</div>
+        <div className="ordering">Ordering</div>
+        <div className="create-btn">Create button</div>
+      </div>
+      <div className="board">
+        {columnNames.map((item) => {
+          return <SingleColumn title={item} />;
         })}
-      </div>  
+      </div>
     </div>
-  )
-}
+  );
+};
